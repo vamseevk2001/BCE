1ee9d6544e5c8a23abd0756394083a97b26f5f07
vamseevk2001 <vamseevk2001@gmail.com>
2022-06-03 10:56:56 +0530

    minor bugs fixed and added data binding for all the fragments

--- app/src/main/java/com/example/bce/GuestList.java
+++ app/src/main/java/com/example/bce/GuestList.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.bce.databinding.FragmentGuestListBinding;
+import com.example.bce.databinding.FragmentHomeFragBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link GuestList#newInstance} factory method to
@@ -15,34 +18,13 @@ import android.view.ViewGroup;
  */
 public class GuestList extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public GuestList() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GuestList.
-     */
-    // TODO: Rename and change types and number of parameters
     public static GuestList newInstance(String param1, String param2) {
         GuestList fragment = new GuestList();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -50,8 +32,7 @@ public class GuestList extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -59,6 +40,7 @@ public class GuestList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guest_list, container, false);
+        FragmentGuestListBinding binding = FragmentGuestListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/bce/profile_frag.java
+++ app/src/main/java/com/example/bce/profile_frag.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.bce.databinding.FragmentHomeFragBinding;
+import com.example.bce.databinding.FragmentProfileFragBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link profile_frag#newInstance} factory method to
@@ -15,33 +18,14 @@ import android.view.ViewGroup;
  */
 public class profile_frag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public profile_frag() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profile_frag.
-     */
-    // TODO: Rename and change types and number of parameters
     public static profile_frag newInstance(String param1, String param2) {
         profile_frag fragment = new profile_frag();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +34,6 @@ public class profile_frag extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -59,6 +41,7 @@ public class profile_frag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_frag, container, false);
+        FragmentProfileFragBinding binding = FragmentProfileFragBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
