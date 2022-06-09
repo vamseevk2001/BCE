f74592ec133051c497de186175576b0c72460766
vamseevk2001 <vamseevk2001@gmail.com>
2022-06-09 12:27:42 +0530

    added API urls

--- app/src/main/java/com/example/bce/Utils/AppUrls.java
+++ app/src/main/java/com/example/bce/Utils/AppUrls.java
@@ -10,4 +10,24 @@ public class AppUrls {
     public static final String UpdateProfileOtherInfo = "https://www.apollohospitalsevent.com/badmin/api.php?method=updateothe";
     public static final String UpdateProfileNominee = "https://www.apollohospitalsevent.com/badmin/api.php?method=updatenominee";
     public static final String UpdateOtherBasic = "https://www.apollohospitalsevent.com/badmin/api.php?method=updateotherbasic";
+
+
+    public static final String BusinessGiven = "https://www.apollohospitalsevent.com/badmin/api.php?method=bussinessgiven";
+    public static final String BusinessReceive	 = "https://www.apollohospitalsevent.com/badmin/api.php?method=bussinessreceive";
+    public static final String SendLead	 = "https://www.apollohospitalsevent.com/badmin/api.php?method=sendlead";
+    public static final String guestList = "https://www.apollohospitalsevent.com/badmin/api.php?method=guestlist";
+    public static final String addGuestList	 = "https://www.apollohospitalsevent.com/badmin/api.php?method=addguestlist";
+    public static final String favoriteConnection = "https://www.apollohospitalsevent.com/badmin/api.php?method=favoriteconnection";
+    public static final String sendThanksNote = "https://www.apollohospitalsevent.com/badmin/api.php?method=sendthanksnote";
+    public static final String reviewList = "https://www.apollohospitalsevent.com/badmin/api.php?method=reviewlist";
+    public static final String sendReview = "https://www.apollohospitalsevent.com/badmin/api.php?method=sendreveiw";
+    public static final String tenderList = "https://www.apollohospitalsevent.com/badmin/api.php?method=helpdesklist";
+    public static final String helpDeskSubmit = "https://www.apollohospitalsevent.com/badmin/api.php?method=helpdesksubmit";
+    public static final String paymentList = "https://www.apollohospitalsevent.com/badmin/api.php?method=paymentlist";
+    public static final String memberAsk = "https://www.apollohospitalsevent.com/badmin/api.php?method=memberasklist";
+    public static final String filterMembershipList = "https://www.apollohospitalsevent.com/badmin/api.php?method=filtermemberlist";
+    public static final String askHistoryList = "https://www.apollohospitalsevent.com/badmin/api.php?method=askhistory";
+    public static final String submitAsk = "https://www.apollohospitalsevent.com/badmin/api.php?method=asksubmit";
+
+
 }
